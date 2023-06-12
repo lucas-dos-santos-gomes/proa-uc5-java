355f10ba795314ad6956780b967c217b76cce7d4
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-12 09:56:36 -0300

    Finalizando exercício do caixa eletrônico

---
+++ Aula4_ExemploCaixaEletronico/src/caixaeletronico/CaixaEletronico.java
@@ -0,0 +1,19 @@
+package caixaeletronico;
+
+public class CaixaEletronico {
+  public static void main(String[] args) {
+    System.out.println("Exemplo de Caixa Eletrônico - Método Construtor");
+    Conta contaLucas = new Conta("Lucas Gomes", 492631);
+    contaLucas.VerSaldo();
+    contaLucas.Saque(10250);
+
+    System.out.println();
+    Conta contaAndreia = new Conta("Andreia Silva", 12543.57);
+    contaAndreia.VerSaldo();
+
+    System.out.println();
+    Conta contaElder = new Conta("Elder Barbosa", 4713.24);
+    contaElder.VerSaldo();
+    contaElder.Saque(8940.97);
+  }
+}
---
+++ Aula4_ExemploCaixaEletronico/src/caixaeletronico/Conta.java
@@ -0,0 +1,31 @@
+package caixaeletronico;
+import java.util.Random;
+
+public class Conta {
+  String titular;
+  int numeroConta;
+  double saldo;
+
+  public Conta(String nomeTitular, double saldoInicial) {
+    this.titular = nomeTitular;
+    this.saldo = saldoInicial;
+
+    Random geraNumero = new Random();
+    this.numeroConta = geraNumero.nextInt(1000000, 9999999);
+  }
+
+  public void VerSaldo() {
+    System.out.println("O número da conta é: " + this.numeroConta);
+    System.out.println("O nome do titular é: " + this.titular);
+    System.out.println("O saldo da conta é: " + this.saldo);
+  }
+
+  public void Saque(double valorSaque) {
+    if(this.saldo >= valorSaque) {
+      this.saldo -= valorSaque;
+      System.out.println("\nValor sacado: " + valorSaque + "\nSaldo atual: " + this.saldo + " reais");
+    } else {
+      System.out.println("\nSaldo insuficiente!");
+    }
+  }
+}
