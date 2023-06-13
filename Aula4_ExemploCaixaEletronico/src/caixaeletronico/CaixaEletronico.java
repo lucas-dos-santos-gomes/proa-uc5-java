94f010f97118fbcf8b37cd1db5335cd1b0edaee8
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-13 01:23:21 -0300

    Finalizando automatização do exercício do caixa eletrônico

--- Aula4_ExemploCaixaEletronico/src/caixaeletronico/CaixaEletronico.java
+++ Aula4_ExemploCaixaEletronico/src/caixaeletronico/CaixaEletronico.java
@@ -3,11 +3,6 @@ package caixaeletronico;
 public class CaixaEletronico {
   public static void main(String[] args) {
     System.out.println("Exemplo de Caixa Eletrônico - Método Construtor");
-    Conta contaLucas = new Conta("Lucas Gomes", 492631);
-    contaLucas.VerSaldo();
-    contaLucas.Saque(10250);
-
-    System.out.println();
     Conta contaAndreia = new Conta("Andreia Silva", 12543.57);
     contaAndreia.VerSaldo();
     contaAndreia.Deposito(62000);
@@ -16,5 +11,9 @@ public class CaixaEletronico {
     Conta contaElder = new Conta("Elder Barbosa", 4713.24);
     contaElder.VerSaldo();
     contaElder.Saque(8940.97);
+
+    System.out.println();
+    Conta contaLucas = new Conta("Lucas Gomes", 492631);
+    contaLucas.Iniciar();
   }
 }
--- Aula4_ExemploCaixaEletronico/src/caixaeletronico/Conta.java
+++ Aula4_ExemploCaixaEletronico/src/caixaeletronico/Conta.java
@@ -1,21 +1,24 @@
 package caixaeletronico;
+
 import java.util.Random;
+import java.util.Scanner;
 
 public class Conta {
   String titular;
   int numeroConta;
   double saldo;
 
+  Random geraNumero = new Random();
+  Scanner entrada = new Scanner(System.in);
+
   public Conta(String nomeTitular, double saldoInicial) {
     this.titular = nomeTitular;
     this.saldo = saldoInicial;
-
-    Random geraNumero = new Random();
     this.numeroConta = geraNumero.nextInt(1000000, 9999999);
   }
 
   public void VerSaldo() {
-    System.out.println("O número da conta é: " + this.numeroConta);
+    System.out.println("\nO número da conta é: " + this.numeroConta);
     System.out.println("O nome do titular é: " + this.titular);
     System.out.println("O saldo da conta é: " + this.saldo);
   }
@@ -36,6 +39,50 @@ public class Conta {
       this.saldo += valorDeposito;
       System.out.println("\nValor depositado: " + valorDeposito);
       System.out.println("Saldo atual: " + this.saldo);
+    }
+  }
+
+  public void Iniciar() {
+    int opcao;
+    do {
+      ExibirMenu();
+      opcao = entrada.nextInt();
+      EscolherFuncao(opcao);
+    } while(opcao != 4);
+  }
+  
+  public void ExibirMenu() {
+    System.out.println("\nBem vindo ao banco H-bank");
+    System.out.println("Escolha sua opção:");
+    System.out.println("1 - Ver saldo");
+    System.out.println("2 - Sacar");
+    System.out.println("3 - Depositar");
+    System.out.println("4 - Finalizar");
+  }
+
+  public void EscolherFuncao(int opcao) {
+    double saque;
+    double deposito;
+
+    switch(opcao) {
+      case 1:
+        VerSaldo();
+        break;
+      case 2:
+        System.out.println("\nDigite o valor a ser sacado:");
+        saque = entrada.nextDouble();
+        Saque(saque);
+        break;
+      case 3:
+        System.out.println("\nDigite o valor que será depositado:");
+        deposito = entrada.nextDouble();
+        Deposito(deposito);
+      case 4:
+        System.out.println("\nOperação finalizada!");
+        break;
+      default:
+        System.out.println("Opção inválida!");
+        break;
     }
   }
 }
